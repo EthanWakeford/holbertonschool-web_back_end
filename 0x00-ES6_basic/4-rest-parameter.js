5e7fc66f4b71220e84166a0c6faac182646a17e8
EthanWakeford <ethanwakeford@gmail.com>
2023-01-06 15:29:54 -0600

    added 4

--- 0x00-ES6_basic/4-rest-parameter.js
+++ 0x00-ES6_basic/4-rest-parameter.js
@@ -1,7 +1,8 @@
+/* eslint-disable no-unused-vars */
 export default function returnHowManyArguments(...args) {
   let count = 0;
   for (const i of args) {
-  count++;
-  };
+    count = count + 1;
+  }
   return count;
 }
