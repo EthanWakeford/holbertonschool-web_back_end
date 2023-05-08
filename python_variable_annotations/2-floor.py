6778dee3622ba7c15ef3308a10278e68e62e413e
EthanWakeford <ethanwakeford@gmail.com>
2023-05-08 12:35:33 -0500

    added task 2

---
+++ python_variable_annotations/2-floor.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""has a functions that returns the floor of a float"""
+
+
+def floor(n: float) -> int:
+    """returns the floor of a float using math.floor"""
+    import math.floor
+    return math.floor(n)
