91ea0663c493daedf2446e72370ee804569fbdaf
EthanWakeford <ethanwakeford@gmail.com>
2023-05-10 14:26:24 -0500

    added task 3

---
+++ python_async_function/3-tasks.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""async python"""
+import asyncio
+wait_random = __import__('0-basic_async_syntax').wait_random
+
+
+def task_wait_random(max_delay: int) -> asyncio.Task:
+    """creates and returns an async task"""
+    return asyncio.create_task(wait_random(max_delay))
