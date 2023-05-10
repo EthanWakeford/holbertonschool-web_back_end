21cf497e9c9ec623a842b60b0ae5e633aacf5dc4
EthanWakeford <ethanwakeford@gmail.com>
2023-05-10 11:55:19 -0500

    line too long

--- python_variable_annotations/9-element_length.py
+++ python_variable_annotations/9-element_length.py
@@ -4,5 +4,6 @@ from typing import Iterable, Sequence, List, Tuple
 
 
 def element_length(lst: Iterable[Sequence]) -> List[Tuple[Sequence, int]]:
-    """returns a list with a tuple containing the len and list of each item in iterable"""
+    """returns a list with a tuple containing the len and list of each item in
+     iterable"""
     return [(i, len(i)) for i in lst]
