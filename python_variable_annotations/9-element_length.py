3261cef2c22e5a705248e091e503beaa2f86f759
EthanWakeford <ethanwakeford@gmail.com>
2023-05-10 11:52:51 -0500

    added task 9

---
+++ python_variable_annotations/9-element_length.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""strongly typed"""
+from typing import Iterable, Sequence, List
+
+
+def element_length(lst: Iterable[Sequence]) -> List[Tuple[Sequence, int]]:
+    """returns a list with a tuple containing the len and list of each item in iterable"""
+    return [(i, len(i)) for i in lst]
