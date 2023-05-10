60af47f00b1b294ce022499baf262fe6118d3db7
EthanWakeford <ethanwakeford@gmail.com>
2023-05-10 13:54:12 -0500

    added task 1

---
+++ python_async_function/1-concurrent_coroutines.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""async couroutieneslnsdrs"""
+import asyncio
+import random
+from typing import List
+wait_random = __import__('0-basic_async_syntax').wait_random
+
+
+async def wait_n(n: int, max_delay: int) -> List[float]:
+    """runs wait_random n number of times and adds all delays into the return list"""
+    delays: List[float] = []
+
+    for future in asyncio.as_completed([wait_random(max_delay) for i in range(n)]):
+        delays.append(await future)
+
+    return delays
