23e063e9c6d5419dec5362c56aa8f6151f1ababc
EthanWakeford <ethanwakeford@gmail.com>
2023-05-10 11:16:01 -0500

    old version of python

--- python_variable_annotations/5-sum_list.py
+++ python_variable_annotations/5-sum_list.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 """strongly typed"""
+from typing import List
 
-
-def sum_list(input_list: list[float]) -> float:
+def sum_list(input_list: List[float]) -> float:
     """returns the sum of all floats in a list"""
     return sum(input_list)
