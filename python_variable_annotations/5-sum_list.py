31dfbdb28253d9f2b3a53946ff437d221a2b92d0
EthanWakeford <ethanwakeford@gmail.com>
2023-05-10 11:13:01 -0500

    fixed type annotation for list

--- python_variable_annotations/5-sum_list.py
+++ python_variable_annotations/5-sum_list.py
@@ -2,6 +2,6 @@
 """strongly typed"""
 
 
-def sum_list(input_list: [float]) -> float:
+def sum_list(input_list: list[float]) -> float:
     """returns the sum of all floats in a list"""
     return sum(input_list)
