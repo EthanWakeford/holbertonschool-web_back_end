37b7687952a02585f0c23dee2e7791e82cce8d12
EthanWakeford <ethanwakeford@gmail.com>
2023-05-11 19:42:05 -0500

    added task 0

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -2,11 +2,11 @@
 """has an async generator"""
 import asyncio
 import random
+from typing import Generator
 
 
-async def async_generator() -> float:
+async def async_generator() -> Generator[float, None, None]:
     """yields random number with awaiting sleep 1 second"""
     for i in range(10):
         await asyncio.sleep(1)
         yield random.uniform(0, 10)
-    return
