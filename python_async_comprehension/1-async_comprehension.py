6db08ae51bfa5cd8d68d825ed756501a1b2817d7
EthanWakeford <ethanwakeford@gmail.com>
2023-05-11 19:52:42 -0500

    added task 1

---
+++ python_async_comprehension/1-async_comprehension.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""collects and returns 10 random numbers using async generator"""
+import asyncio
+from typing import List
+async_generator = __import__('0-async_generator').async_generator
+
+
+async def async_comprehension() -> List[float]:
+    """uses an async comprehesion to create a list of random numbers"""
+    return [i async for i in async_generator()]
