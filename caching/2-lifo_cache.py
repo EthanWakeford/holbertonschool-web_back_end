59235b18a73a15000938700ac859847e54c5c5f6
EthanWakeford <ethanwakeford@gmail.com>
2023-05-15 14:56:02 -0500

    added task 2

---
+++ caching/2-lifo_cache.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+"""implements a caching class from a base class"""
+BaseCaching = __import__('base_caching').BaseCaching
+
+
+class LIFOCache(BaseCaching):
+    """a lifo cache example"""
+
+    def put(self, key, item):
+        """puts into cache, removes first key if cache is full"""
+        if not key or not item:
+            return
+        self.cache_data[key] = item
+
+        if len(self.cache_data) > self.MAX_ITEMS:
+            last_key = self.cache_data.popitem()
+            print('DISCARD: ' + last_key)
+
+    def get(self, key):
+        """gets value from cache"""
+        if not key or key not in self.cache_data:
+            return None
+        return self.cache_data[key]
