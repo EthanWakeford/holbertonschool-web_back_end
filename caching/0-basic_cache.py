963106fcbd71587109a45df52f3c346eb5c2be83
EthanWakeford <ethanwakeford@gmail.com>
2023-05-15 14:00:35 -0500

    added task 0

--- caching/0-basic_cache.py
+++ caching/0-basic_cache.py
@@ -7,6 +7,14 @@ class BasicCache(BaseCaching):
     """basic cache class"""
 
     def put(self, key, item):
+        """puts into cache"""
         if not key or not item:
             return
         self.cache_data[key] = item
+
+
+    def get(self, key):
+        """gets from cache"""
+        if not key or key not in self.cache_data:
+            return None
+        return self.cache_data[key]
