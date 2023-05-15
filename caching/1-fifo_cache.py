bd2347c97a776e4e3ff0378f9e5839282883c62e
EthanWakeford <ethanwakeford@gmail.com>
2023-05-15 14:42:57 -0500

    added task 1

---
+++ caching/1-fifo_cache.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+"""implements a caching class from a base class"""
+BaseCaching = __import__('base_caching').BaseCaching
+
+
+class FIFOCache(BaseCaching):
+    """a fifo cache example"""
+
+    def put(self, key, item):
+        """puts into cache, removes first key if cache is full"""
+        if not key or not item:
+            return
+        self.cache_data[key] = item
+        
+        if self.MAX_ITEMS > 4:
+            first_key = next(iter(self.cache_data))
+            self.cache_data.pop(first_key)
+            print('DISCARD: ' + first_key)
+
+
+    def get(self, key):
+        """gets value from cache"""
+        if not key or key not in self.cache_data:
+            return None
+        return self.cache_data[key]
