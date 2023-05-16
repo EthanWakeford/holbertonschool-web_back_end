3594b9c92ade0487a70607b4ec260d05fccb68da
EthanWakeford <ethanwakeford@gmail.com>
2023-05-16 13:29:54 -0500

    funciton docs

--- pagination/0-simple_helper_function.py
+++ pagination/0-simple_helper_function.py
@@ -4,5 +4,6 @@ from typing import Tuple
 
 
 def index_range(page: int, page_size: int) -> Tuple[int, int]:
+    """creates a range of the indexes for pagination"""
     end: int = (page * page_size)
     return (end - page_size, end)
