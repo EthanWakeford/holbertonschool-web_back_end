37803d49c89bbb297f8dc90db70270b795999490
EthanWakeford <ethanwakeford@gmail.com>
2023-05-22 15:15:04 -0500

    added type annotations

--- personal_data/filtered_logger.py
+++ personal_data/filtered_logger.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python3
 """returns a log"""
 import re
+from typing import List
 
 
-def filter_datum(fields, redaction, message, separator):
+def filter_datum(fields: List[str], redaction: str, message: str, separator: str) -> str:  # noqa
     """filters the message and takes out info listed in the fields"""
     return re.sub(r'(' + '|'.join(fields) + r')=[^{}]+'.format(separator), r'\1={}'.format(redaction), message)  # noqa
