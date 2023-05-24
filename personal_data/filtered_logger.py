8ad89631181555a6b2b30ac2a51cb0610a010ec9
EthanWakeford <ethanwakeford@gmail.com>
2023-05-23 20:45:44 -0500

    added task 1

--- personal_data/filtered_logger.py
+++ personal_data/filtered_logger.py
@@ -7,3 +7,20 @@ from typing import List
 def filter_datum(fields: List[str], redaction: str, message: str, separator: str) -> str:  # noqa
     """filters the message and takes out info listed in the fields"""
     return re.sub(r'(' + '|'.join(fields) + r')=[^{}]+'.format(separator), r'\1={}'.format(redaction), message)  # noqa
+
+
+class RedactingFormatter(logging.Formatter):
+    """ Redacting Formatter class
+        """
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields: List[str]):
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+
+    def format(self, record: logging.LogRecord) -> str:
+        """formats a log message"""
+        log_message: str = super().format(record)
+        return filter_datum(self.fields, self.REDACTION, message, self.SEPARATOR)  # noqa
