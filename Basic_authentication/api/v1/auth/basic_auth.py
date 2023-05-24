2d35e6e2da452555c3e8d2fba00c8c62e5ab06d2
EthanWakeford <ethanwakeford@gmail.com>
2023-05-24 15:49:41 -0500

    added base 64 decoding

--- Basic_authentication/api/v1/auth/basic_auth.py
+++ Basic_authentication/api/v1/auth/basic_auth.py
@@ -2,6 +2,7 @@
 """basic auth class"""
 from .auth import Auth
 import re
+import base64
 
 
 class BasicAuth(Auth):
@@ -14,3 +15,13 @@ class BasicAuth(Auth):
         if not re.search('^Basic ', authorization_header):
             return None
         return re.sub('^Basic ', '', authorization_header)
+
+    def decode_base64_authorization_header(self, base64_authorization_header: str) -> str:  # noqa
+        """returns the decoded value of a Base64 string"""
+        if base64_authorization_header is None or type(base64_authorization_header) != str:  # noqa
+            return None
+
+        try:
+            return base64.b64decode(base64_authorization_header).decode('utf-8')  # noqa
+        except Exception:
+            return None
