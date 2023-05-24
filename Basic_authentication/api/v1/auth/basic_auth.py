7034314d5ee4f5c43458c0114086ebc9b9acae9d
EthanWakeford <ethanwakeford@gmail.com>
2023-05-24 15:36:38 -0500

    added base 64 header extractor

--- Basic_authentication/api/v1/auth/basic_auth.py
+++ Basic_authentication/api/v1/auth/basic_auth.py
@@ -1,7 +1,15 @@
 #!/usr/bin/env python3
 """basic auth class"""
 from .auth import Auth
+import re
 
 
 class BasicAuth(Auth):
     """basic auth class inherits from auth"""
+
+    def extract_base64_authorization_header(self, authorization_header: str) -> str:  # noqa
+        if authorization_header is None or type(authorization_header) != str:
+            return None
+        if not re.search('^Basic ', authorization_header):
+            return None
+        return re.sub('^Basic ', '', authorization_header)
