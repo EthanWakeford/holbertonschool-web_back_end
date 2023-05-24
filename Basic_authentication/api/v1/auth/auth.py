056aa0238df3d15a5f2395d53c128cfbe56b5e74
EthanWakeford <ethanwakeford@gmail.com>
2023-05-24 14:35:03 -0500

    added require auth method

--- Basic_authentication/api/v1/auth/auth.py
+++ Basic_authentication/api/v1/auth/auth.py
@@ -2,14 +2,19 @@
 """holds an auth class"""
 from flask import request
 from typing import List, TypeVar
+import os.path
 
 
 class Auth():
     """an authorization class"""
 
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
-        """auth auth auth"""
-        return False
+        """checks to see if path is in excluded_paths"""
+        if not path or not excluded_paths:
+            return True
+        if os.path.join(path, '') in excluded_paths:
+            return False
+        return True
 
     def authorization_header(self, request=None) -> str:
         """header header header"""
