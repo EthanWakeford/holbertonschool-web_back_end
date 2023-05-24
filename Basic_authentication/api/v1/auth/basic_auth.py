5991477f2a802e4a19673b34eb57c19cabb81e6a
EthanWakeford <ethanwakeford@gmail.com>
2023-05-24 16:21:39 -0500

    added task 10

--- Basic_authentication/api/v1/auth/basic_auth.py
+++ Basic_authentication/api/v1/auth/basic_auth.py
@@ -3,6 +3,8 @@
 from .auth import Auth
 import re
 import base64
+from models.user import User
+from typing import TypeVar
 
 
 class BasicAuth(Auth):
@@ -33,3 +35,15 @@ class BasicAuth(Auth):
         if not re.search(':', decoded_base64_authorization_header):
             return None, None
         return tuple(re.split(':', decoded_base64_authorization_header))
+
+    def user_object_from_credentials(self, user_email: str, user_pwd: str) -> TypeVar('User'):  # noqa
+        """returns the User instance based on his email and password"""
+        if user_email is None or type(user_email) != str:
+            return None
+        if user_pwd is None or type(user_pwd) != str:
+            return None
+
+        user = User.search({'email': user_email})
+        if len(user) is 0 or user[0].is_valid_password(user_pwd):
+            return None
+        return user[0]
