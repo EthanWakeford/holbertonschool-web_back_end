a633c551f0fde662292b3dd58423fc92834a4ef1
EthanWakeford <ethanwakeford@gmail.com>
2023-05-24 14:13:57 -0500

    added task 2 auth dir

---
+++ Basic_authentication/api/v1/auth/auth.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+"""holds an auth class"""
+from flask import request
+
+
+class Auth():
+    """an authorization class"""
+
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """auth"""
+        return False
+
+    def authorization_header(self, request=None) -> str:
+        """header"""
+        return None
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        """user"""
+        return None
