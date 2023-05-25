ce7834add7717b0e37311d0837a7e003f7b5c31d
EthanWakeford <ethanwakeford@gmail.com>
2023-05-24 23:38:21 -0500

    changed return annotation

--- personal_data/encrypt_password.py
+++ personal_data/encrypt_password.py
@@ -3,7 +3,7 @@
 import bcrypt
 
 
-def hash_password(password: str) -> str:
+def hash_password(password: str) -> bytes:
     """hashes a password"""
     salt = bcrypt.gensalt()
     hashed_password = bcrypt.hashpw(password.encode(), salt)
