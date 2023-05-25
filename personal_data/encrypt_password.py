3b037fa4ddcb9c380912b40bb3f258ca5cebec6e
EthanWakeford <ethanwakeford@gmail.com>
2023-05-24 23:37:30 -0500

    added task 5

---
+++ personal_data/encrypt_password.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""encrypts passwords"""
+import bcrypt
+
+
+def hash_password(password: str) -> str:
+    """hashes a password"""
+    salt = bcrypt.gensalt()
+    hashed_password = bcrypt.hashpw(password.encode(), salt)
+    return hashed_password
