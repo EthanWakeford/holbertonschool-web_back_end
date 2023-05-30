e19be9bca0934c2e61e41f47941d755651980dba
EthanWakeford <ethanwakeford@gmail.com>
2023-05-30 16:02:50 -0500

    added a basic flask app

---
+++ user_authentication_service/app.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+"""flask app"""
+from os import getenv
+from flask import Flask, jsonify, abort, request
+from flask_cors import (CORS, cross_origin)
+import os
+
+
+app = Flask(__name__)
+CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
+
+@app.route('/', methods=['GET'])
+def root():
+    """root of flask app"""
+    jsonify({"message": "Bienvenue"})
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port="5000")
