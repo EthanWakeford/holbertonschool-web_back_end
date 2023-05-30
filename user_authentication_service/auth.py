6ebe2d9caae228c2a0f36540af32d17df96d1d03
EthanWakeford <ethanwakeford@gmail.com>
2023-05-30 15:48:07 -0500

    task 5

--- user_authentication_service/auth.py
+++ user_authentication_service/auth.py
@@ -2,6 +2,34 @@
 """does some authenticating"""
 import bcrypt
 import base64
+from db import DB
+from user import Base, User
+from sqlalchemy.orm.exc import NoResultFound
+
+
+class Auth:
+    """Auth class to interact with the authentication database.
+    """
+
+    def __init__(self):
+        self._db = DB()
+
+    def register_user(self, email: str, password: str) -> User:
+        """adds a user into the db"""
+        db = DB()
+
+        try:
+            db.find_user_by(email=email)
+            # print('found me a user')
+            raise ValueError(f'User {email} already exists')
+        except NoResultFound:
+            # print("no results")
+            hashed_password = _hash_password(password)
+            db.add_user(email, hashed_password)
+            user = db.find_user_by(email=email)
+            # print(user.id, user.email)
+        
+        return User
 
 
 def _hash_password(password: str) -> bytes:
