7ec4f7674aa55be40e9856ba18df957ef190732d
EthanWakeford <ethanwakeford@gmail.com>
2023-05-30 16:41:26 -0500

    added create session method

--- user_authentication_service/auth.py
+++ user_authentication_service/auth.py
@@ -36,6 +36,16 @@ class Auth:
         except Exception as e:
             return False
 
+    def create_session(self, email: str) -> str:
+        """creates a session id for a user based on their email"""
+        try:
+            user = self._db.find_user_by(email=email)
+            session_id = _generate_uuid()
+            self._db.update_user(user.id, session_id=session_id)
+            return session_id
+        except Exception:
+            return None
+
 
 def _hash_password(password: str) -> bytes:
     """hashes a password"""
