42f9d2451d310eff6619d1c42a3d1d8998fec00a
EthanWakeford <ethanwakeford@gmail.com>
2023-05-30 16:16:37 -0500

    added users endpoint to create new user

--- user_authentication_service/app.py
+++ user_authentication_service/app.py
@@ -4,7 +4,10 @@ from os import getenv
 from flask import Flask, jsonify, abort, request
 from flask_cors import (CORS, cross_origin)
 import os
+from auth import Auth
 
+
+AUTH = Auth()
 
 app = Flask(__name__)
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
@@ -15,6 +18,19 @@ def root():
     """root of flask app"""
     return jsonify({"message": "Bienvenue"})
 
+
+@app.route('/users', methods=['POST'])
+def users():
+    """registers a user"""
+    email = request.form.get('email')
+    password = request.form.get('password')
+    print(email, password)
+
+    try:
+        AUTH.register_user(email, password)
+        return jsonify({"email": email, "message": "user created"})
+    except Exception:
+        return jsonify({"message": "email already registered"}), 400
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
