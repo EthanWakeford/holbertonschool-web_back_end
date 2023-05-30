ec1216de1fc840ff782ec55da23ad176f80187a5
EthanWakeford <ethanwakeford@gmail.com>
2023-05-30 12:49:43 -0500

    added method to get cookie from reqeust

--- Session_authentication/api/v1/auth/auth.py
+++ Session_authentication/api/v1/auth/auth.py
@@ -3,6 +3,7 @@
 from flask import request
 from typing import List, TypeVar
 import os.path
+from os import getenv
 
 
 class Auth():
@@ -27,3 +28,11 @@ class Auth():
     def current_user(self, request=None) -> TypeVar('User'):
         """user user user"""
         return None
+
+    def session_cookie(self, request=None):
+        """ returns a cookie value from a request"""
+        if request is None:
+            return None
+        session_name = getenv('SESSION_NAME', '_my_session_id')
+        return request.cookies.get(session_name)
+        
