fbfed77a50b4b585ff92734909f22c6d160bdfcd
EthanWakeford <ethanwakeford@gmail.com>
2023-05-30 18:20:31 -0500

    added /sessions delete route

--- user_authentication_service/app.py
+++ user_authentication_service/app.py
@@ -47,5 +47,17 @@ def sessions():
     return response
 
 
+@app.route('/sessions', methods=['DELETE'])
+def logout():
+    """destroys a session"""
+    session_id = request.cookies.get('session_id')
+    user = AUTH.get_user_from_session_id(session_id)
+    if user is None:
+        abort(403)
+
+    AUTH.destroy_session(user.id)
+    root()
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
