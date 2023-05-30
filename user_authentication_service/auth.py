31206abfebc774ed753d8cc5b3913a892e9507c4
EthanWakeford <ethanwakeford@gmail.com>
2023-05-30 17:42:43 -0500

    updated to validate user_id first

--- user_authentication_service/auth.py
+++ user_authentication_service/auth.py
@@ -56,8 +56,11 @@ class Auth:
 
     def destroy_session(self, user_id: int) -> None:
         """updates a users session id to None"""
-        self._db.update_user(user_id, session_id=None)
-        return None
+        try:
+            self._db.find_user_by(user_id=user_id)
+            self._db.update_user(user_id, session_id=None)
+        except Exception:
+            return None
 
 
 def _hash_password(password: str) -> bytes:
