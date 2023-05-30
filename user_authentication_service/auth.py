6176d97e8c8d655573e4917216c85c200ad752f9
EthanWakeford <ethanwakeford@gmail.com>
2023-05-30 17:44:23 -0500

    added task 13

--- user_authentication_service/auth.py
+++ user_authentication_service/auth.py
@@ -57,7 +57,7 @@ class Auth:
     def destroy_session(self, user_id: int) -> None:
         """updates a users session id to None"""
         try:
-            self._db.find_user_by(user_id=user_id)
+            self._db.find_user_by(id=user_id)
             self._db.update_user(user_id, session_id=None)
         except Exception:
             return None
