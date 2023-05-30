f0510c1efac40370c235e244fd90e5f411701666
EthanWakeford <ethanwakeford@gmail.com>
2023-05-30 17:38:16 -0500

    added return None

--- user_authentication_service/auth.py
+++ user_authentication_service/auth.py
@@ -57,6 +57,7 @@ class Auth:
     def destroy_session(self, user_id: int) -> None:
         """updates a users session id to None"""
         self._db.update_user(user_id, session_id=None)
+        return None
 
 
 def _hash_password(password: str) -> bytes:
