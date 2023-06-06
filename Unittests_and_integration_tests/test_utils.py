51db2b367f3c7577d5f15e9850f65cb77a0d3c3a
EthanWakeford <ethanwakeford@gmail.com>
2023-06-06 14:57:37 -0500

    task 0

--- Unittests_and_integration_tests/test_utils.py
+++ Unittests_and_integration_tests/test_utils.py
@@ -8,10 +8,10 @@ import utils
 class TestAccessNestedMap(unittest.TestCase):
     """testing for access_nest_map method"""
     @parameterized.expand([
-        ('test one', {"a": 1}, ("a",), 1),
-        ('test 2', {"a": {"b": 2}}, ("a",), {"b": 2}),
-        ('test 3', {"a": {"b": 2}}, ("a", "b"), 2)
+        ({"a": 1}, ("a",), 1),
+        ({"a": {"b": 2}}, ("a",), {"b": 2}),
+        ({"a": {"b": 2}}, ("a", "b"), 2)
     ])
-    def test_access_nested_map(self, name, nest_map, path, expected):
+    def test_access_nested_map(self, nest_map, path, expected):
         """testing method"""
         self.assertEqual(utils.access_nested_map(nest_map, path), expected)
