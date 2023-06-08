03d31d09bcaf06a055e97d8dbb4b745f1fedb528
EthanWakeford <ethanwakeford@gmail.com>
2023-06-08 09:49:01 -0500

    added task 3

--- Unittests_and_integration_tests/test_utils.py
+++ Unittests_and_integration_tests/test_utils.py
@@ -42,3 +42,25 @@ class TestGetJson(unittest.TestCase):
             response = utils.get_json(url)
             mock_get.assert_called_once_with(url)
             self.assertEqual(response, payload)
+
+
+class TestMemoize(unittest.TestCase):
+    """tests memoize function"""
+    def test_memoize(self):
+        """Tests `memoize` function using a TestClass class"""
+        
+        class TestClass:
+    
+            def a_method(self):
+                return 42
+        
+            @utils.memoize
+            def a_property(self):
+                return self.a_method()
+    
+        with patch.object(TestClass, 'a_method', return_value=42) as mock_method:  # noqa
+            test_class = TestClass()
+            self.assertEqual(test_class.a_property, 42)
+            self.assertEqual(test_class.a_property, 42)
+            mock_method.assert_called_once()
+    
