132e15b59d7cb020d8d690a78cdab3b92f4680c3
EthanWakeford <ethanwakeford@gmail.com>
2023-06-13 12:45:23 -0500

    added task 0

--- MySQL_Advanced/0-uniq_users.sql
+++ MySQL_Advanced/0-uniq_users.sql
@@ -1,8 +1,8 @@
---creates a table onto any database
+-- creates a table onto any database
 
-CREATE TABLE IF NOT EXISTS 'users' (
+CREATE TABLE IF NOT EXISTS users (
     id int,
-    email varchar(255) NOT NULL ,
-    UNIQUE (EMAIL)
-    name varchar(255)
+    email varchar(255) NOT NULL,
+    name varchar(255),
+    UNIQUE (email)
 );    
