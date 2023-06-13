dee07535e6c3be94a2e45d56e627c352923e9617
EthanWakeford <ethanwakeford@gmail.com>
2023-06-13 12:54:51 -0500

    added task 0

--- MySQL_Advanced/0-uniq_users.sql
+++ MySQL_Advanced/0-uniq_users.sql
@@ -4,6 +4,6 @@ CREATE TABLE IF NOT EXISTS users (
     id int NOT NULL AUTO_INCREMENT,
     email varchar(255) NOT NULL,
     name varchar(255),
-    UNIQUE (email)
+    UNIQUE (email),
     PRIMARY KEY (id)
 );    
