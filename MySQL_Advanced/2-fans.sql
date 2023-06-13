34f03ab5895b4217d3b8358b498136902a71c30c
EthanWakeford <ethanwakeford@gmail.com>
2023-06-13 14:08:58 -0500

    added task 2

--- MySQL_Advanced/2-fans.sql
+++ MySQL_Advanced/2-fans.sql
@@ -1,3 +1,6 @@
 -- lists fans by country of origin
 
-SELECT origin, fans FROM metal_bands GROUP BY origin;
+SELECT origin, SUM(fans) as nb_fans
+    FROM metal_bands
+    GROUP BY origin
+    ORDER BY nb_fans DESC;
