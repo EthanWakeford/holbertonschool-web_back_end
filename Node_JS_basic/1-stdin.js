af50fe31e9aa45c5aa9ccb207670f2769ca26358
EthanWakeford <ethanwakeford@gmail.com>
2023-06-26 17:03:14 -0500

    added ask 1

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,6 +1,7 @@
 console.log('Welcome to Holberton School, what is your name?');
 process.stdin.on('data', (data) => {
-  console.log(`Your name is: ${data}`);
+  process.stdout.write(`Your name is: ${data}`);
+  process.exit()
 })
 
 process.on('exit', () => {
