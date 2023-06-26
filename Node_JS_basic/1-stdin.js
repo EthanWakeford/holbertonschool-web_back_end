554ddc4de7019002e8061f780d19f1b1a7d22170
EthanWakeford <ethanwakeford@gmail.com>
2023-06-26 17:10:12 -0500

    fixed for eslint

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,9 +1,9 @@
 console.log('Welcome to Holberton School, what is your name?');
 process.stdin.on('data', (data) => {
   process.stdout.write(`Your name is: ${data}`);
-  process.exit()
-})
+  process.exit();
+});
 
 process.on('exit', () => {
   console.log('This important software is now closing');
-})
+});
