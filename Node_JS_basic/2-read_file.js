a4c5eaf99f82e25f702d6330f0b58320ab6fca46
EthanWakeford <ethanwakeford@gmail.com>
2023-06-26 18:16:49 -0500

    added task 2

--- Node_JS_basic/2-read_file.js
+++ Node_JS_basic/2-read_file.js
@@ -7,26 +7,26 @@ function countStudents(path) {
     const fields = {
       CS: {
         studentCount: 0,
-        students: []
+        students: [],
       },
       SWE: {
         studentCount: 0,
-        students: []
+        students: [],
       },
     };
-    
+
     for (const row of data.split('\n').slice(1, -1)) {
       const student = row.split(',');
       fields[student[3]].studentCount += 1;
-      fields[student[3]].students.push(student[0])
-      count++;
+      fields[student[3]].students.push(student[0]);
+      count += 1;
     }
-      
+
     console.log(`Number of students: ${count}`);
-    console.log(`Number of students in CS: ${ fields.CS.studentCount }.List: { fields.CS.students }`)
-    console.log(`Number of students in SWE: ${ fields.SWE.studentCount }.List: { fields.SWE.students }`)
+    console.log(`Number of students in CS: ${fields.CS.studentCount}.List: ${fields.CS.students.join(', ')}`);
+    console.log(`Number of students in SWE: ${fields.SWE.studentCount}.List: ${fields.SWE.students.join(', ')}`);
   } catch (err) {
-    throw new Error(err);
+    throw new Error('Cannot load the database');
   }
 }
 
