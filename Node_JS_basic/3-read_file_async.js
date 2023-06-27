8e4bae998c0504fbcf3e3bbfead5fd59621b1b85
EthanWakeford <ethanwakeford@gmail.com>
2023-06-27 13:59:09 -0500

    added task 3

--- Node_JS_basic/3-read_file_async.js
+++ Node_JS_basic/3-read_file_async.js
@@ -1,6 +1,4 @@
-// const { rejects } = require('assert');
 const fs = require('fs');
-// const { resolve } = require('path');
 
 function countStudents(path) {
   return new Promise((resolve, reject) => {
@@ -17,23 +15,23 @@ function countStudents(path) {
             students: [],
           },
         };
-    
+
         for (const row of data.split('\n').slice(1, -1)) {
           const student = row.split(',');
           fields[student[3]].studentCount += 1;
           fields[student[3]].students.push(student[0]);
           count += 1;
         }
-    
+
         console.log(`Number of students: ${count}`);
         console.log(`Number of students in CS: ${fields.CS.studentCount}. List: ${fields.CS.students.join(', ')}`);
         console.log(`Number of students in SWE: ${fields.SWE.studentCount}. List: ${fields.SWE.students.join(', ')}`);
-        resolve()
-      })
-    } catch(err) {
+        resolve();
+      });
+    } catch (err) {
       reject(err);
     }
-  })
-  }
+  });
+}
 
 module.exports = countStudents;
