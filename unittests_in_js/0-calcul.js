4d0bcad0d422079c6704d26549f494e77dd26498
EthanWakeford <ethanwakeford@gmail.com>
2023-06-28 14:24:54 -0500

    added task 0

--- unittests_in_js/0-calcul.js
+++ unittests_in_js/0-calcul.js
@@ -1,8 +1,5 @@
-// rounds a and b and returns the sum
 function calculateNumber(a, b) {
-  const roundedA = Math.round(a);
-  const roundedB = Math.round(b);
-  return roundedA + roundedB;
+  return Math.round(a) + Math.round(b);
 }
 
 module.exports = calculateNumber;
