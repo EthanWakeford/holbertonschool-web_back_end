c31200b711b71fa433912a88d985584eb1ab56ba
EthanWakeford <ethanwakeford@gmail.com>
2023-06-28 20:29:22 -0500

    task 8 setup

---
+++ Node_JS_basic/full_server/utils.js
@@ -0,0 +1,24 @@
+function readDatabase(path) {
+  return fs
+    .readFile(path, 'utf8')
+    .then((data) => {
+      const fields = {
+        CS: {
+          students: [],
+        },
+        SWE: {
+          students: [],
+        },
+      };
+
+      for (const row of data.split('\n').slice(1, -1)) {
+        const student = row.split(',');
+        fields[student[3]].students.push(student[0]);
+      }
+
+      return fields;
+    })
+    .catch(() => {
+      throw new Error('Cannot load the database');
+    });
+}
