3bbdaa67f6b472bf4b637d99e9133b9c55108084
EthanWakeford <ethanwakeford@gmail.com>
2023-07-03 20:15:20 -0500

    added task 5

---
+++ queuing_system_in_js/5-subscriber.js
@@ -0,0 +1,20 @@
+import { createClient, print } from "redis";
+import { promisify } from "util";
+
+const client = createClient();
+
+client.on("error", (err) =>
+  console.log("Redis client not connected to the server:", err)
+);
+
+console.log("Redis client connected to the server");
+
+client.subscribe("holberton school channel");
+
+client.on("message", (channel, message) => {
+  console.log(message);
+  if (message == "KILL_SERVER") {
+    client.unsubscribe();
+    client.quit();
+  }
+});
