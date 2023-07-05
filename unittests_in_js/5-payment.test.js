0d5cffd0d8ccd00c2640f79b4a3bb17df7524f40
EthanWakeford <ethanwakeford@gmail.com>
2023-07-05 12:25:46 -0500

    added task 5

---
+++ unittests_in_js/5-payment.test.js
@@ -0,0 +1,27 @@
+const sinon = require("sinon");
+const { expect } = require("chai");
+const Utils = require("./utils.js");
+const sendPaymentRequestToApi = require("./3-payment.js");
+
+describe("sendPaymentRequestToApi", () => {
+  let consoleSpy;
+    beforeEach(() => {
+        consoleSpy = sinon.spy(console, 'log');
+    });
+
+    afterEach(() => {
+        consoleSpy.restore();
+    });
+
+    it('checks if the console.log is called with the right arg', () => {
+        const res = sendPaymentRequestToApi(100, 20);
+        expect(consoleSpy.calledOnceWithExactly('The total is: 120')).to.be.true;
+        expect(res).to.equal(120);
+    });
+
+    it('checks if the console.log is called with the right arg', () => {
+        const res = sendPaymentRequestToApi(10, 10);
+        expect(consoleSpy.calledOnceWithExactly('The total is: 20')).to.be.true;
+        expect(res).to.equal(20);
+    });
+});
