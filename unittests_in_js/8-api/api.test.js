0537af9c7e29a0eafd07adb434a0b3a304b9fc56
EthanWakeford <ethanwakeford@gmail.com>
2023-07-05 13:12:33 -0500

    added task 9

--- unittests_in_js/8-api/api.test.js
+++ unittests_in_js/8-api/api.test.js
@@ -3,11 +3,25 @@ const { response } = require("express");
 const request = require("request");
 
 describe("testing get request", () => {
-  it("returns correct string and 200 status", (done) => {
+  it("returns correct string and 200 status", (done
+    ) => {
     request("http://localhost:7865/", (err, res, body) => {
       expect(res.statusCode).to.equal(200)
       expect(body).to.equal('Welcome to the payment system')
       done();
     })
   });
+  it('works with int as id', (done) => {
+    request('http://localhost:7865/cart/2', (err, res, body) => {
+      expect(res.statusCode).to.equal(200);
+      expect(body).to.equal('Payment methods for cart 2');
+      done();
+    })
+  })
+  it('does not work with not int as id', (done) => {
+    request('http://localhost:7865/cart/notanumber', (err , res, body) => {
+      expect(res.statusCode).to.equal(404);
+      done();
+    });
+  })
 });
