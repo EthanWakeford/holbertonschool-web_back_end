8ffbc22ac2a67007d121453d8375f29fba69cd33
EthanWakeford <ethanwakeford@gmail.com>
2023-07-05 13:04:57 -0500

    added test

--- unittests_in_js/8-api/api.test.js
+++ unittests_in_js/8-api/api.test.js
@@ -3,10 +3,11 @@ const { response } = require("express");
 const request = require("request");
 
 describe("testing get request", () => {
-  it("returns correct string and 200 status", () => {
+  it("returns correct string and 200 status", (done) => {
     request("http://localhost:7865/", (err, res, body) => {
       expect(res.statusCode).to.equal(200)
       expect(body).to.equal('Welcome to the payment system')
+      done();
     })
   });
 });
