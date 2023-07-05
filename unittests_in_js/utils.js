e82a26b815d0f727de5c361a970c3946fe56a496
EthanWakeford <ethanwakeford@gmail.com>
2023-07-05 11:24:22 -0500

    added utils

---
+++ unittests_in_js/utils.js
@@ -0,0 +1,19 @@
+const Utils = {
+  calculateNumber: function(type, a, b) {
+    roundedA = Math.round(a);
+    roundedB = Math.round(b);
+  
+    if (type === 'SUM') {
+      return roundedA + roundedB;
+    } else if (type === 'SUBTRACT') {
+      return roundedA - roundedB;
+    } else {
+      if (roundedB === 0) {
+        return 'Error';
+      }
+      return roundedA / roundedB;
+    }
+  }
+}
+
+module.exports = Utils;
